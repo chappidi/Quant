db09e75350c0e255979e0cf106c7587709fbf336
chappidi <chappidi@live.com>
2018-05-07 08:33:57 -0400

    Tick Data

--- quant.core/Common/Tick.cs
+++ quant.core/Common/Tick.cs
@@ -4,33 +4,42 @@ using System.Text;
 
 namespace quant.common
 {
+    public enum Aggressor { NA, Buy, Sell }
     /// <summary>
     /// represents tick data of a given Security
     /// </summary>
     public class Tick
     {
-        public enum Aggressor { NA, Buy, Sell }
         #region ctor
-        public Tick(Security sec, uint qty, uint price, DateTime time) {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sec"></param>
+        /// <param name="qty"></param>
+        /// <param name="price">in qep format</param>
+        /// <param name="atTS"></param>
+        public Tick(Security sec, uint qty, uint price, DateTime atTS) {
             Security = sec;
             Quantity = qty;
             Price = price;
-            Time = time;
+            TradedAt = atTS;
         }
+        #endregion
+        #region properties
+        public Security     Security    { get; }
+        public uint         Quantity    { get; }
+        public uint         Price       { get; }
+        public DateTime     TradedAt    { get; }
+        public Aggressor    Side        { get; set; } = Aggressor.NA;
+        public bool         Live        { get; set; } = false;
+        public double       PxVol => Price * Quantity;
         #endregion
 
-        #region properties
-        public Security Security { get; }
-        public uint Quantity { get; }
-        public uint Price { get; }
-        public DateTime Time { get; }
-        public Aggressor Side { get; set; } = Aggressor.NA;
-        public bool Live { get; set; } = false;
-        public double PxVol => Price * Quantity;
-        #endregion
+        #region Object
         public override string ToString() {
-            var tm = Time.ToString("MM/dd/yyyy HH:mm:ss.fff");
-            return $"[{Security}\t{tm}\t{Quantity}\t{Price}]";
+            var tm = TradedAt.ToString("MM/dd/yyyy HH:mm:ss.fff");
+            return $"[{Security}\t{tm}\t{Quantity.ToString("N0").PadLeft(3)}\t{Price}]";
         }
+        #endregion
     }
 }

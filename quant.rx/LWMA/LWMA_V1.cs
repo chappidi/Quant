30c65dc09693d9314f408e2d7ab7e814421236b7
chappidi <chappidi@live.com>
2018-09-15 21:21:30 -0400

    LWMA

---
+++ quant.rx/LWMA/LWMA_V1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reactive.Linq;
+using System.Text;
+
+namespace quant.rx
+{
+    /// <summary>
+    /// Basic Implementation
+    /// </summary>
+    internal static partial class LWMAV1Ext
+    {
+        internal static IObservable<double> LWMA_V1(this IObservable<double> source, int period)
+        {
+            uint weight = 0;
+            for (uint i = 1; i <= period; ++i) {
+                weight += i;
+            }
+
+            return source.Buffer(period, 1).Where(x => x.Count == period).Select(x => {
+                double total = 0;
+                for (int i = 0; i < period; i++) {
+                    total += x[i] * (i + 1);
+                }
+                return total / weight;
+            });
+        }
+    }
+}
---
+++ quant.rx/LWMA/LWMA_V2.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reactive.Disposables;
+using System.Text;
+
+namespace quant.rx
+{
+    class LWMA_V2 : IObservable<double>
+    {
+        readonly IObservable<double> _source;
+        readonly IObservable<double> _offset;
+        readonly uint _period;
+        readonly uint m_weight = 0;
+        //Variables
+        readonly RingWnd<double> _ring = null;
+        double _total = 0;
+        double _weighted = 0;
+        uint _count = 0;
+
+        #region ctor
+        public LWMA_V2(IObservable<double> source, uint period, IObservable<double> offset)
+        {
+            _source = source;
+            _offset = offset;
+            _period = period;
+            for(uint i = 1; i <= period; ++i) {
+                m_weight += i;
+            }
+
+            _ring = new RingWnd<double>(period);
+        }
+        #endregion
+        void OnVal(double newVal, double oldVal, IObserver<double> obsvr)
+        {
+            // Step 1: calculate
+            _weighted += _period * newVal - _total;
+            if (_count < _period) {
+                _count++;
+                _total += newVal;
+            } else {
+                _total += newVal - oldVal;
+            }
+            // Step 2: publish
+            if (_count == _period)
+                obsvr.OnNext(_weighted / m_weight);
+        }
+        #region IObservable
+        public IDisposable Subscribe(IObserver<double> obsvr) { 
+            var ret = new CompositeDisposable();
+            // offset calculations
+            if (_offset != null) {
+                ret.Add(_offset.Subscribe(ofst => {
+                    // empty to do ????? 
+                }));
+            }
+            ret.Add(_source.Subscribe(val => OnVal(val, _ring.Enqueue(val), obsvr), obsvr.OnError, obsvr.OnCompleted));
+            return ret;
+        }
+        #endregion
+    }
+    internal static partial class LWMAV2Ext
+    {
+        internal static IObservable<double> LWMA_V2(this IObservable<double> source, uint period, IObservable<double> offset = null)
+        {
+            return new LWMA_V2(source, period, offset);
+        }
+    }
+}

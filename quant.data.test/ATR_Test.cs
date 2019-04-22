888fc46a9081dcb4b1d64090e9605796f429756f
chappidi <chappidi@live.com>
2019-04-21 22:55:34 -0400

    new tests

--- quant.data.test/ATR_Test.cs
+++ quant.data.test/ATR_Test.cs
@@ -15,20 +15,56 @@ namespace quant.data.test
     {
         static string prdt = "ES";
         static DateTime dtStart = DateTime.Parse("03-13-2018");
+        static TimeSpan timeSpan = TimeSpan.FromMinutes(10);
+        static IEnumerator<(string, DateTime)> roll = DataSource.Query(prdt).GetEnumerator();
+        OHLC ohlc = null;
+        IObservable<OHLC> raw = DataSource.Query(prdt, dtStart, dtStart.AddDays(1), obs => obs.Stitch(roll)).ByInterval(timeSpan);
+        IObservable<OHLC> cnt = DataSource.Query(prdt, dtStart, dtStart.AddDays(1)).ByInterval(timeSpan);
+
+        [TestMethod]
+        public void SMA_TEST()
+        {
+            var res1 = raw.Do(x => ohlc = x).SMA(14).Select(x => new { OHLC = ohlc, SMA = x }).ToList().Wait();
+            var res2 = cnt.Do(x => ohlc = x).SMA(14).Select(x => new { OHLC = ohlc, SMA = x }).ToList().Wait();
+            for (var itr = 0; itr < res1.Count; itr++)
+            {
+                Trace.WriteLine($"{res1[itr].OHLC.Close.Security}\t{res1[itr].SMA.ToString("0.00")}\t{res2[itr].OHLC.Close.Security}\t{res2[itr].SMA.ToString("0.00")}\t{(res2[itr].SMA - res1[itr].SMA).ToString("0.00")}");
+            }
+        }
+        /// <summary>
+        /// Validate EMA using Continuous pricing vs Discrete Roll 
+        /// </summary>
+        [TestMethod]
+        public void EMA_TEST_2()
+        {
+            var res1 = raw.Do(x => ohlc = x).EMA(14).Select(x => new { OHLC = ohlc, EMA = x }).ToList().Wait();
+            var res2 = cnt.Do(x => ohlc = x).EMA(14).Select(x => new { OHLC = ohlc, EMA = x }).ToList().Wait();
+            for (var itr = 0; itr < res1.Count; itr++)
+            {
+                Trace.WriteLine($"{res1[itr].OHLC.Close.Security}\t{res1[itr].EMA.ToString("0.00")}\t{res2[itr].OHLC.Close.Security}\t{res2[itr].EMA.ToString("0.00")}\t{(res2[itr].EMA - res1[itr].EMA).ToString("0.00")}");
+            }
+        }
+        /// <summary>
+        /// Validate WSMA using Continuous pricing vs Discrete Roll 
+        /// </summary>
+        [TestMethod]
+        public void WSMA_TEST_2()
+        {
+            var res1 = raw.Do(x => ohlc = x).WSMA(14).Select(x => new { OHLC = ohlc, WSMA = x }).ToList().Wait();
+            var res2 = cnt.Do(x => ohlc = x).WSMA(14).Select(x => new { OHLC = ohlc, WSMA = x }).ToList().Wait();
+            for (var itr = 0; itr < res1.Count; itr++)
+            {
+                Trace.WriteLine($"{res1[itr].OHLC.Close.Security}\t{res1[itr].WSMA.ToString("0.00")}\t{res2[itr].OHLC.Close.Security}\t{res2[itr].WSMA.ToString("0.00")}\t{(res2[itr].WSMA - res1[itr].WSMA).ToString("0.00")}");
+            }
+        }
         [TestMethod]
         public void ATR_TEST_2()
         {
-            var roll = DataSource.Query(prdt).GetEnumerator();
-            var timeSpan = TimeSpan.FromMinutes(10);
-            OHLC ohlc = null;
-
-            var abc = DataSource.Query(prdt, dtStart, dtStart.AddDays(1), obs => obs.Stitch(roll)).ByInterval(timeSpan).Do(x => ohlc = x).TR().WSMA(14);
-            var xyz = DataSource.Query(prdt, dtStart, dtStart.AddDays(1)).ByInterval(timeSpan).Do(x => ohlc = x).TR().WSMA(14);
-            var res1 = abc.Select(x => new { OHLC = ohlc, ATR = x }).ToList().Wait();
-            var res2 = xyz.Select(x => new { OHLC = ohlc, ATR = x }).ToList().Wait();
+            var res1 = raw.Do(x => ohlc = x).TR().WSMA(14).Select(x => new { OHLC = ohlc, ATR = x }).ToList().Wait();
+            var res2 = cnt.Do(x => ohlc = x).TR().WSMA(14).Select(x => new { OHLC = ohlc, ATR = x }).ToList().Wait();
             for (var itr = 0; itr < res1.Count; itr++)
             {
-                Trace.WriteLine($"{res1[itr].OHLC.Close.Security.Symbol}\t{res1[itr].ATR.ToString("0.00")}\t{res2[itr].OHLC.Close.Security.Symbol}\t{res2[itr].ATR.ToString("0.00")}\t{(res2[itr].ATR - res1[itr].ATR).ToString("0.00")}");
+                Trace.WriteLine($"{res1[itr].OHLC.Close.Security}\t{res1[itr].ATR.ToString("0.00")}\t{res2[itr].OHLC.Close.Security}\t{res2[itr].ATR.ToString("0.00")}\t{(res2[itr].ATR - res1[itr].ATR).ToString("0.00")}");
             }
         }
     }
